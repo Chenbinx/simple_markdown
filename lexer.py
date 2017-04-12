cd4717662865cd92a5a2d41b609b88724d3f35bc
Chenbin <chen.bin11@zte.com.cn>
2017-04-12 11:16:59 +0800

    完成lexer

--- lexer.py
+++ lexer.py
@@ -1,16 +1,21 @@
 import re
-import sys
 
 
-(TITLE, ORDER_LIST, UNORDER_LIST, STRONG, EMPHASIS, STRING) = \
-    ('TITLE', 'ORDER_LIST', 'UNORDER_LIST', 'STRONG', 'EMPHASIS', 'STRING')
+(TITLE, ORDER_LIST, UNORDER_LIST, STRONG, EMPHASIS,
+ LINK, IMAGE, CODE, STRING, NEWLINE) = \
+    ('TITLE', 'ORDER_LIST', 'UNORDER_LIST', 'STRONG', 'EMPHASIS',
+     'LINK', 'IMAGE', 'CODE', 'STRING', 'NEWLINE')
 
 MARKDOWN_PATTERNS = [
-    (r'^#+\s+', TITLE),
-    (r'^\s?\+\s+', UNORDER_LIST),
-    (r'^\s?[0-9]+\.\s+', ORDER_LIST),
+    (r'#+[ \t]+', TITLE),
+    (r'[ \t]?\+[ \t]+', UNORDER_LIST),
+    (r'[ \t]?\d+\.[ \t]+', ORDER_LIST),
     (r'\*\*[^\*]+\*\*', STRONG),
-    # (r'\*[^\*]+*', EMPHASIS),
+    (r'\*[^\*]+\*', EMPHASIS),
+    (r'!\[.+\]\(.+\)', IMAGE),
+    (r'\[.+\]\(.+\)', LINK),
+    (r'`{3}[ \t]*', CODE),
+    (r'(\n|\r\n)', NEWLINE),
 ]
 
 
@@ -32,16 +37,13 @@ class Lexer(object):
     def __init__(self, text):
         self.text = text
 
-    def lex(self, patterns):
-        """
-        patterns params list
-        """
+    def lex(self):
         pos = 0
         tokens = []
         not_match = []
         while pos < len(self.text):
-            match = None            
-            for token_expr in patterns:
+            match = None
+            for token_expr in MARKDOWN_PATTERNS:
                 pattern, tag = token_expr
                 regex = re.compile(pattern)
                 match = regex.match(self.text, pos)
@@ -61,3 +63,38 @@ class Lexer(object):
         if len(not_match) != 0:
             tokens.append(Token(STRING, ''.join(not_match)))
         return tokens
+
+
+def main():
+    r = Lexer('''
+# Windows comp
+
+    + [intro](#1)
+    + [hello](#2)
+    + [chap](#3)
+    + [ref](#3)
+
+1. hello
+2. world
+
+## <h2 id="1">intro</h2>
+
+    introduce the production
+
+example:
+
+```
+guard @check voiagent else {
+    @svn checkout http://xx.xx.x.xxx/svn/ZXCCP-VPlat_xxxx/trunk/source.repo/src/vx.x/windows/voiagent/
+}
+```
+
+PROJECT_DIR = @search_file_path voiagent.sln
+OUTPUT_DIR = $PROJECT_DIR\Output\
+INSTALLER_DIR = @search_file_path voi_windows_client_setup.nsi
+''').lex()
+    for a in r:
+        print(a)
+
+if __name__ == '__main__':
+    main()
--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -5,23 +5,53 @@ from lexer import *
 
 class TestLexer(unittest.TestCase):
     def test_title_lex(self):
-        self.assertEqual(Lexer('# hello').lex(MARKDOWN_PATTERNS),
+        self.assertEqual(Lexer('# hello').lex(),
                          [Token(TITLE, '# '), Token(STRING, 'hello')])
-        self.assertEqual(Lexer('## hello world again').lex(MARKDOWN_PATTERNS),
+        self.assertEqual(Lexer('## hello world again').lex(),
                          [Token(TITLE, '## '), Token(STRING, 'hello world again')])
 
     def test_order_list_lex(self):
-        self.assertEqual(Lexer('1. hello').lex(MARKDOWN_PATTERNS),
+        self.assertEqual(Lexer('1. hello').lex(),
                          [Token(ORDER_LIST, '1. '), Token(STRING, 'hello')])
 
     def test_unorder_list_lex(self):
-        self.assertEqual(Lexer('+  hello').lex(MARKDOWN_PATTERNS),
+        self.assertEqual(Lexer('+  hello').lex(),
                          [Token(UNORDER_LIST, '+  '), Token(STRING, 'hello')])
 
     def test_strong_lex(self):
-        for a in Lexer('hello **chenbin** again').lex(MARKDOWN_PATTERNS):
-            print(a)
-        self.assertEqual(Lexer('hello **chenbin** again').lex(MARKDOWN_PATTERNS),
+        self.assertEqual(Lexer('hello **chenbin** again').lex(),
                          [Token(STRING, 'hello '),
                           Token(STRONG, '**chenbin**'),
                           Token(STRING, ' again')])
+
+    def test_emphasis_lex(self):
+        self.assertEqual(Lexer('hello *chenbin* again').lex(),
+                         [Token(STRING, 'hello '),
+                          Token(EMPHASIS, '*chenbin*'),
+                          Token(STRING, ' again')])
+
+    def test_link_lex(self):
+        self.assertEqual(Lexer('hello [link](www.zte.com.cn)').lex(),
+                         [Token(STRING, 'hello '),
+                          Token(LINK, '[link](www.zte.com.cn)')])
+
+    def test_image_lex(self):
+        self.assertEqual(Lexer('hello ![link](www.zte.com.cn)').lex(),
+                         [Token(STRING, 'hello '),
+                          Token(IMAGE, '![link](www.zte.com.cn)')])
+
+    def test_code_lex(self):
+        self.assertEqual(Lexer('''```
+func() {
+    print("hello")
+}
+```''').lex(),
+                         [Token(CODE, '```'),
+                          Token(NEWLINE, '\n'),
+                          Token(STRING, 'func() {'),
+                          Token(NEWLINE, '\n'),
+                          Token(STRING, '    print("hello")'),
+                          Token(NEWLINE, '\n'),
+                          Token(STRING, '}'),
+                          Token(NEWLINE, '\n'),
+                          Token(CODE, '```')])
